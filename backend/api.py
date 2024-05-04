f2e02a75f1369fbeefbd5d892b0cdb3f7c7ffc48
derklausberger <niklasklausberger@gmail.com>
2024-05-04 13:53:00 +0200

    req working

--- backend/api.py
+++ backend/api.py
@@ -1,13 +1,29 @@
 import requests
+import os
 
-API_KEY = ""
-GOOGLE_BOOKS_URL = "https://www.googleapis.com/books/v1/volumes?q="
+var_name = "ISBN_DB_API_KEY"
+if var_name in os.environ:
+    var_value = os.environ[var_name]
 
-def get_book(book_id):
-    response = requests.get(GOOGLE_BOOKS_URL + book_id)
+GOOGLE_BOOKS_URL = "https://www.googleapis.com/books/"
+
+def get_book_google(book_id):
+    response = requests.get(GOOGLE_BOOKS_URL + "v1/volumes?q=" + book_id)
+
     if response.status_code == 200:
-        print(response.text)
+        data = response.json()
+
+        for book in data['items']:
+            if 'volumeInfo' in book:
+                if 'title' in book['volumeInfo']:
+                    print(book['volumeInfo']['title'], end='')
+                else:
+                    print("Title key does not exist for this book")
+                
+                if 'subtitle' in book['volumeInfo']:
+                    print(": " + book['volumeInfo']['title'], end='')
+                print()
     else:
         print('Fehler beim GET-Request:', response.status_code)
 
-get_book("Öffentliches+Recht+Ein+Basislehrbuch")
+get_book_google("Detterbeck%2C+Steffen%3A+Öffentliches+Recht+–+Ein+Basislehrbuch+zum+Staatsrecht%2C+Verwaltungsrecht+und+Europarecht+mit+Übungsfällen")
