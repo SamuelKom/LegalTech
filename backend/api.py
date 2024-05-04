263746ec026e3089d6c32ed456e88ede27dc7b26
gbjk1 <klausguntram@icloud.com>
2024-05-04 14:03:21 +0200

    credential management

--- backend/api.py
+++ backend/api.py
@@ -1,29 +1,38 @@
 import requests
 import os
+from dotenv import load_dotenv
 
-var_name = "ISBN_DB_API_KEY"
-if var_name in os.environ:
-    var_value = os.environ[var_name]
+load_dotenv('.env') 
+ISBN_DB_API_KEY = os.environ.get('ISBN_DB_API_KEY')
 
-GOOGLE_BOOKS_URL = "https://www.googleapis.com/books/"
+print(ISBN_DB_API_KEY)
 
-def get_book_google(book_id):
-    response = requests.get(GOOGLE_BOOKS_URL + "v1/volumes?q=" + book_id)
 
-    if response.status_code == 200:
-        data = response.json()
 
-        for book in data['items']:
-            if 'volumeInfo' in book:
-                if 'title' in book['volumeInfo']:
-                    print(book['volumeInfo']['title'], end='')
-                else:
-                    print("Title key does not exist for this book")
+# var_name = "ISBN_DB_API_KEY"
+# if var_name in os.environ:
+#     var_value = os.environ[var_name]
+
+# GOOGLE_BOOKS_URL = "https://www.googleapis.com/books/"
+# ISBN_DB_API_URL = "https://api2.isbndb.com"
+
+# def get_book_google(book_id):
+#     response = requests.get(GOOGLE_BOOKS_URL + "v1/volumes?q=" + book_id)
+
+#     if response.status_code == 200:
+#         data = response.json()
+
+#         for book in data['items']:
+#             if 'volumeInfo' in book:
+#                 if 'title' in book['volumeInfo']:
+#                     print(book['volumeInfo']['title'], end='')
+#                 else:
+#                     print("Title key does not exist for this book")
                 
-                if 'subtitle' in book['volumeInfo']:
-                    print(": " + book['volumeInfo']['title'], end='')
-                print()
-    else:
-        print('Fehler beim GET-Request:', response.status_code)
+#                 if 'subtitle' in book['volumeInfo']:
+#                     print(": " + book['volumeInfo']['title'], end='')
+#                 print()
+#     else:
+#         print('Fehler beim GET-Request:', response.status_code)
 
-get_book_google("Detterbeck%2C+Steffen%3A+Öffentliches+Recht+–+Ein+Basislehrbuch+zum+Staatsrecht%2C+Verwaltungsrecht+und+Europarecht+mit+Übungsfällen")
+# get_book_google("Detterbeck%2C+Steffen%3A+Öffentliches+Recht+–+Ein+Basislehrbuch+zum+Staatsrecht%2C+Verwaltungsrecht+und+Europarecht+mit+Übungsfällen")
